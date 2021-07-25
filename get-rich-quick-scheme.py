3e90d76842efac7839c3e67ab01824139a9ec733
Basil Schneider <basil.schneider@bluewin.ch>
2021-07-25 02:48:15 +0200

    Functionality to place multiple bets at once

--- get-rich-quick-scheme.py
+++ get-rich-quick-scheme.py
@@ -10,39 +10,73 @@ from kellyBet import kellyBet
 class get_rich_quick_scheme():
 
     def __init__(self):
+
         self.api_key = os.environ.get('binance_api')
         self.api_secret = os.environ.get('binance_secret')
 
         self.client = Client(self.api_key, self.api_secret)
         self.dry_run = True
 
-        # The system can keep track of exactly one buy and exactly one sell
-        # order
+        # Create a dict of 2-tuples (key = symbol;
+        #                            value = (price_precision, quantity_precision))
+        self.symbol_precisions = {}
+        futures_exchange_info = self.client.futures_exchange_info()
+        for info in futures_exchange_info['symbols']:
+            self.symbol_precisions[info['symbol']] = (info['pricePrecision'],
+                                                      info['quantityPrecision']
+                                                     )
+
+        # Keep track of multiple buy and sell orders by lists
+        # self.order_ids is a dict of dicts, e.g.:
+        # self.order_ids = {1: {'BUY': 1, 'SELL': 2}, 2: {'BUY': 98, 'SELL': 99}}
         # An order ID of -1  means there is no current order registered in the
         # system
-        self.order_id_buy = -1
-        self.order_id_sell = -1
+        self.order_ids = {}
 
     def config_logger(self):
         logging.basicConfig(level=logging.INFO,
-                            #format='%(name)s - %(levelname)7s - %(asctime)s - %(message)s',
-                            format='%(levelname)-7s - %(asctime)s - %(message)s',
+                            #format='%(name)s - %(levelname)8s - %(asctime)s - %(message)s',
+                            format='%(levelname)-8s - %(asctime)s - %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
 
-    def check_open_order(self):
-        if self.order_id_buy < 0 and self.order_id_sell < 0:
-            logging.info('No current orders found.')
+    def initialize_order_ids(self, idx, buy_id=-1, sell_id=-1):
+        self.order_ids[idx] = {'BUY': buy_id, 'SELL': sell_id}
+
+    def set_buy_order_id(self, idx, buy_id=-1):
+        try:
+            self.order_ids[idx]['BUY'] = buy_id
+        except KeyError:
+            self.initialize_order_ids(idx, buy_id=buy_id)
+
+    def set_sell_order_id(self, idx, sell_id=-1):
+        try:
+            self.order_ids[idx]['SELL'] = sell_id
+        except KeyError:
+            self.initialize_order_ids(idx, sell_id=sell_id)
+
+    def check_open_order(self, idx):
+        try:
+            # If order IDs are -1, there are no current orders for a given
+            # index
+            if self.order_ids[idx]['BUY'] < 0 and \
+               self.order_ids[idx]['SELL'] < 0:
+                logging.info('No current orders found. [index=%d]')
+                return False
+        except KeyError:
+            # If order IDs are unset, there are no current orders for a given
+            # index
             return False
-        logging.info('Current BUY order ID: %s', self.order_id_buy)
-        logging.info('Current SELL order ID: %s', self.order_id_sell)
+
+        # If we reach here, order IDs are both set and positive,
+        # which means we have valid current orders
+        logging.info('Current BUY order ID: %s [index=%d]', self.order_ids[idx]['BUY'])
+        logging.info('Current SELL order ID: %s [index=%d]', self.order_ids[idx]['SELL'])
         return True
 
-    def reset_open_order(self):
-        logging.info('Reset BUY order ID %s.' % self.order_id_buy)
-        logging.info('Reset SELL order ID %s.' % self.order_id_sell)
-        self.order_id_buy = -1
-        self.order_id_sell = -1
-
+    def reset_open_order(self, idx):
+        logging.info('Reset BUY order ID %s [index=%d].' % self.order_ids[idx]['BUY'])
+        logging.info('Reset SELL order ID %s [index=%d].' % self.order_ids[idx]['SELL'])
+        self.order_ids[idx]['BUY'] = self.order_ids[idx]['SELL'] = -1
 
     def turn_off_dry_run(self):
         self.dry_run = False
@@ -86,7 +120,7 @@ class get_rich_quick_scheme():
                     .strftime('%Y-%m-%d %H:%M:%S.%f')
                    )
 
-            logging.info('    Symbol: %s', symbol)
+            logging.info('--  Symbol: %s', symbol)
             logging.info('    Leverage: %d/%d', leverage, leverage_max)
             logging.info('    Entry price: %.2f', price_entry)
             logging.info('    Market price: %.2f', price_market)
@@ -122,7 +156,7 @@ class get_rich_quick_scheme():
                             )
             client_order_id = open_order['clientOrderId']
 
-            logging.info('    Symbol: %s', symbol)
+            logging.info('--  Symbol: %s', symbol)
             logging.info('    Market price: %.2f', price_market)
             logging.info('    Sell price: %.2f', price_sell)
             logging.info('    Quantity: %.3g', quantity)
@@ -131,9 +165,13 @@ class get_rich_quick_scheme():
 
         return len(open_orders)
 
-    def place_new_kelly_bet(self, symbol, leverage):
+    def place_kelly_bet(self, symbol, leverage, idx):
 
-        logging.info('Placing Kelly bet for %s.', symbol)
+        # Set margin type and leverage
+        #self.client.futures_change_margin_type(symbol=symbol, marginType='ISOLATED')
+        self.client.futures_change_leverage(symbol=symbol, leverage=leverage)
+
+        logging.info('Placing Kelly bet for %s [index=%d].', symbol, idx)
         logging.info('Kelly options:')
         wallet_total, wallet_free = self.get_balance('USDT')
         price_market = float(self.client.futures_position_information(symbol=symbol)[0]['markPrice'])
@@ -150,17 +188,27 @@ class get_rich_quick_scheme():
 
         logging.info('Kelly plan:')
 
+        # Get precisions right for Binance API
+        futures_buy = round(myBet.futures_buy,
+                            self.symbol_precisions[symbol][1])
+        futures_sell = round(myBet.futures_sell,
+                             self.symbol_precisions[symbol][1])
+        price_old = round(myBet.price_old,
+                          self.symbol_precisions[symbol][0])
+        price_new = round(myBet.price_new,
+                          self.symbol_precisions[symbol][0])
+
         # Buy futures
         logging.info('    Buy %s futures at %s, pay initial margin %s.',
-                     myBet.futures_buy, myBet.price_old, myBet.asset_old)
+                     futures_buy, price_old, myBet.asset_old)
         if not self.dry_run:
             response = self.client.futures_create_order(symbol=symbol,
                                                         side='BUY',
                                                         type='MARKET',
-                                                        quantity=myBet.futures_buy
+                                                        quantity=futures_buy
                                                        )
-            self.order_id_buy = response['orderId']
-            logging.info('    BUY order ID: %s' % self.order_id_buy)
+            self.set_buy_order_id(idx, buy_id = response['orderId'])
+            logging.info('    BUY order ID: %s' % self.order_ids[idx]['BUY'])
         else:
             logging.warning('Dry run, do not actually buy anything.')
 
@@ -175,8 +223,8 @@ class get_rich_quick_scheme():
 
         # Place sell order
         logging.info('    Sell %s futures at %s (+%.1f %%), get %s, gain %s (+%.1f %% / ROE: +%.1f %%).',
-                     myBet.futures_sell, myBet.price_new,
-                     100*myBet.price_new/myBet.price_old-100,
+                     futures_sell, price_new,
+                     100*price_new/price_old-100,
                      myBet.asset_new, myBet.gain,
                      100*myBet.gain/myBet.asset_total,
                      100*myBet.gain/myBet.asset_old)
@@ -184,58 +232,84 @@ class get_rich_quick_scheme():
             response = self.client.futures_create_order(symbol=symbol,
                                                         side='SELL',
                                                         type='LIMIT',
-                                                        quantity=myBet.futures_sell,
+                                                        quantity=futures_sell,
                                                         timeInForce='GTC', # Good til canceled
-                                                        price=myBet.price_new
+                                                        price=price_new
                                                        )
-            self.order_id_sell = response['orderId']
-            logging.info('    SELL order ID: %s' % self.order_id_sell)
+            self.set_sell_order_id(idx, sell_id = response['orderId'])
+            logging.info('    SELL order ID: %s' % self.order_ids[idx]['SELL'])
 
         # Info about liquidation
         logging.info('    Or %s futures are liquidated at ~%s (%.1f %%), lose %s (-100.0 %% / ROE: -%.2f %%).',
-                     myBet.futures_sell, myBet.price_liq,
-                     100*myBet.price_liq/myBet.price_old-100,
+                     futures_sell, myBet.price_liq,
+                     100*myBet.price_liq/price_old-100,
                      myBet.asset_total,
                      100*myBet.asset_total/myBet.asset_old)
 
     def check_sell_order_status(self):
 
         all_orders = looseitall.client.futures_get_all_orders()
-        for order in all_orders:
-            if order['orderId'] == self.order_id_sell:
-                if order['status'] == 'NEW':
-                    logging.info('Sell order with ID %s still open.' %
-                                 self.order_id_sell)
-                elif order['status'] == 'FILLED':
-                    logging.info('Sell order filled.')
-                    self.reset_open_order()
-                elif order['status'] == 'CANCELED':
-                    logging.info('Sell order canceled.')
-                    self.reset_open_order()
+        # Loop over current orders (stored in this instance)
+        for idx, order_id in self.order_ids.items():
+            # Loop over all orders (from API)
+            for order in all_orders:
+                # Match the two orders
+                if order['orderId'] == order_id['SELL']:
+
+                    # We found the current order for a given index
+                    # Now check status
+                    if order['status'] == 'NEW':
+                        logging.info('Sell order with ID %s still open [index=%d].'
+                                     % (order_id['SELL'], idx))
+                    elif order['status'] == 'FILLED':
+                        logging.info('Sell order withd ID %s filled [index=%d].'
+                                     % (order_id['SELL'], idx))
+                        self.reset_open_order(idx)
+                    elif order['status'] == 'CANCELED':
+                        logging.info('Sell order withd ID %s canceled [index=%d].'
+                                     % (order_id['SELL'], idx))
+                        self.reset_open_order(idx)
 
 
 if __name__ == '__main__':
 
+    # Variables
+    idxs = [55, 77, 99]
+    leverage = 100
+    wallets = [100, 100, 200]
+
+    # Create object
     looseitall = get_rich_quick_scheme()
+
+    # Configure logger
     looseitall.config_logger()
+
+    # Turn off dry run
     looseitall.turn_off_dry_run()
 
+    # Go into an endless loop
     while True:
+
+        # Get number of open positions and open orders
         nOpenPositions = looseitall.show_open_positions()
         nOpenOrders = looseitall.show_open_orders()
 
-        # Check status of current orders and reset them if necessary
+        # Check status of all current orders and reset them if necessary
         looseitall.check_sell_order_status()
 
-        # Check if we have current orders
-        if not looseitall.check_open_order():
-            # If we don't have current orders, place a new one
-            looseitall.place_new_kelly_bet('ETHUSDT', 100)
+        # Place several bets
+        for idx in idxs:
+
+            # Check if we have current orders
+            if not looseitall.check_open_order(idx):
+
+                # If we don't have current orders, place a new one
+                looseitall.place_kelly_bet('ETHUSDT', leverage, idx)
 
         ## If we don't have open positions nor orders, we want to place a new bet
         #if nOpenPositions+nOpenOrders == 0:
         ##if True:
-        #    looseitall.place_new_kelly_bet('ETHUSDT', 100)
+        #    looseitall.place_kelly_bet('ETHUSDT', 100)
 
         sleep(60)
 
@@ -243,6 +317,7 @@ if __name__ == '__main__':
     #print(client.get_asset_balance(asset='ETH'))
     #print(client.get_margin_account())
     #print(client.get_symbol_ticker(symbol="ETHUSDT"))
+    #print(client.get_symbol_info(symbol="ETHUSDT"))
     #print(client.get_open_orders())
     #print(client.futures_account())
     #print(client.futures_account_balance())
