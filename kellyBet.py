59d450ff5eb288f424864aac95710319eb49ca2a
Michael Wettstein <chischte@gmx.ch>
2021-08-18 11:12:50 +0200

    BKP

--- kellyBet.py
+++ kellyBet.py
@@ -99,7 +99,7 @@ class kellyBet:
 if __name__ == '__main__':
 
     myBet = kellyBet(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
-    myBet.kellyBet(1.4, 5.)
+    myBet.kellyBet(1.2, 3.)
     myBet.kellyBetInfo()
     #myBet.kellyBet(3.5, 2.)
-    myBet.kellyBetInfo()
+    #myBet.kellyBetInfo()
